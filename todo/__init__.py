6bb0428ef0d705d412a504bb30ae1c0c04aa6cde
prajingn <prajin376@gmail.com>
2025-11-01 17:47:40 +0530

    Add form to delete all tasks. Add form to add a task.
    Added routes for task addition and droping all
    tasks

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from datetime import date
+from datetime import date, datetime
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
 db = SQLAlchemy(app)
 curr_date = date.today().strftime("%b %d %Y")
+
+with app.app_context():
+    db.create_all()
+
 
 from todo import routes
--- todo/routes.py
+++ todo/routes.py
@@ -1,11 +1,29 @@
-from todo import app, curr_date
-from flask import render_template
+from todo import app, curr_date, date, db, datetime
+from flask import render_template, request, redirect, url_for
+from .models import Task
 
 @app.route("/")
-@app.route("/home")
+@app.route("/pending")
 def pending():
-    return render_template("pending.html", curr_date=curr_date)
-
+    return render_template("pending.html", curr_date=curr_date, date=date, tasks=Task.query.filter(Task.is_complete == False).all())
+        
 @app.route("/complete")
 def complete():
-    return render_template("complete.html", curr_date=curr_date)
+    return render_template("complete.html", curr_date=curr_date, tasks=Task.query.filter(Task.is_complete == True).all())
+
+@app.route("/add_task", methods=["POST"])
+def add_task():
+    t1 = Task(name=request.form["name"],
+            details=request.form["details"],
+            priority=request.form["priority"],
+            due = datetime.strptime(request.form["due"], "%Y-%m-%d").date(),
+            is_complete = False)
+    db.session.add(t1)
+    db.session.commit()
+    return redirect(url_for("pending"))
+
+@app.route("/clear_all", methods=["POST"])
+def clear_all():
+    db.drop_all()
+    db.create_all()
+    return redirect(url_for("pending"))
